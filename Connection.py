4162214e50742fd270cc8505f7b27e443c22ea47
Tammana Triveni <128072417+TRIVENI0971@users.noreply.github.com>
2025-05-09 20:22:58 +0530

    Delete Connection.py

--- Connection.py
+++
@@ -1,8 +0,0 @@
-import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=r"C:\Users\Tammana Triveni\Downloads\black-seer-454106-e1-4bafb35faab6.json"
-
-
-print(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
-
-BQ_PROJECT = "black-seer-454106-e1"
-BQ_DATASET = "DataPipeline"
